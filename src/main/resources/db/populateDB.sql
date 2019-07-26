f77e8f43a24a80103effc81e41fa6388725d0d50
timurorbit <t.hollow@mail.ru>
2019-07-26 15:07:54 +0700

    initDB and populateDB with meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories) VALUES
+  ('1970-01-01 00:00:01', 'one', 201),
+  ('1970-01-01 00:00:01', 'two', 202),
+  ('1970-01-01 00:00:01', 'three', 203),
+  ('1970-01-02 00:00:01', 'four', 2004),
+  ('1970-01-02 00:00:01', 'five', 205),
+  ('1970-01-02 00:00:01', 'six', 206);
